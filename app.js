2bc46c369941450e4d2bc826f1354ce6096a9ff9
Hubert <36423@student.anstar.edu.pl>
2024-10-11 10:54:36 +0200

    Add min, max feature

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ function srednia(arr) {
     return _.mean(arr)
 }
 
+function min(arr) {
+    return _.min(arr)
+}
+
+function max(arr) {
+    return _.max(arr)
+}
+
 console.log(srednia([1,2,3,4,5]));
